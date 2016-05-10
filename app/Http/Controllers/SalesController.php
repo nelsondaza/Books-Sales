bb1f9cce4a84958ddf6b98ef7eebf8a5b5ce48ba
Nelson Daza <nelson.daza@gmail.com>
2016-05-09 22:42:48 -0500

    Create Sales, Delete Sale Test create Sale Test delete Sale

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Books;
 use App\Models\Sales;
 use Illuminate\Http\Request;
 
@@ -9,83 +10,132 @@ use App\Http\Requests;
 
 class SalesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return $this->response( Sales::all() );
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return $this->response(Sales::all());
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validateRequest($request);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $sale = Sales::find( $id );
-        if( $sale )
-            return $this->response( $sale );
-        return $this->responseFail( 'Sale not found.', 404 );
+		/**
+		 * available book?.
+		 */
+		$book = Books::find($request->get('book_id'));
+		if ($book->units_available <= 0)
+			return $this->responseFail('Validation Error.', 422, 422, ['book_id' => 'No units available of this book.']);
 
-    }
+		$sale = Sales::create($request->all());
+		$book->units_available--;
+		$book->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+		return $this->response($sale);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$sale = Sales::find($id);
+		if ($sale)
+			return $this->response($sale);
+		return $this->responseFail('Sale not found.', 404);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$sale = Sales::find($id);
+
+		if ($sale) {
+			if ($sale->delete()) {
+
+				/**
+				 * Now we have one available book, right?.
+				 */
+				$book = $sale->book;
+				$book->units_available++;
+				$book->save();
+
+				return $this->response($sale);
+			}
+			return $this->responseFail("Action not allowed.", 403);
+		}
+
+		return $this->responseFail("Sale not found.", 404);
+	}
+
+	/**
+	 * Validate form data
+	 * @param Request $request
+	 */
+	private function validateRequest(Request $request)
+	{
+		$this->validate($request, [
+			'book_id' => 'required|exists:books,id',
+			'price' => 'required|numeric|min:1',
+		], [
+			'required' => 'The :attribute field is required.',
+			'books_id.exists' => 'Not an existing book for this sale.',
+			'min' => 'The :attribute field needs at least :min chars.',
+			'numeric' => 'The :attribute field needs to be numeric.',
+			'price.min' => 'The :attribute needs to be equals or bigger than 1.',
+		]);
+	}
+
 }
--- tests/SalesTest.php
+++ tests/SalesTest.php
@@ -6,62 +6,113 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class SalesTest extends TestCase
 {
-    private $structure = [
-        'id',
-        'book_id',
-        'price',
-        'created_at',
-        'updated_at',
-    ];
+	private $structure = [
+		'id',
+		'book_id',
+		'price',
+		'created_at',
+		'updated_at',
+	];
 
-    /**
-     * Test sales list.
-     *
-     * @return void
-     */
-    public function testListSales()
-    {
-        $this->get('/api/sales')->seeJsonStructure([
-            'error',
-            'data' => [
-                '*' => $this->structure
-            ]
-        ])->seeJson([
-            'error' => null,
-        ]);
-    }
+	/**
+	 * Test sales list.
+	 *
+	 * @return void
+	 */
+	public function testListSales()
+	{
+		$this->get('/api/sales')->seeJsonStructure([
+			'error',
+			'data' => [
+				'*' => $this->structure
+			]
+		])->seeJson([
+			'error' => null,
+		]);
+	}
 
-    /**
-     * Test view one sale
-     *
-     * @return void
-     */
-    public function testViewSale()
-    {
+	/**
+	 * Test view one sale
+	 *
+	 * @return void
+	 */
+	public function testViewSale()
+	{
 
-        $sale = \App\Models\Sales::orderByRaw("RAND()")->first();
-        $this->get('/api/sales/' . $sale->id )->seeJsonStructure([
-            'error',
-            'data' => [
-                $this->structure
-            ]
-        ])->seeJson([
-            'error' => null,
-            'id' => $sale->id
-        ]);
+		$sale = \App\Models\Sales::orderByRaw("RAND()")->first();
+		$this->get('/api/sales/' . $sale->id)->seeJsonStructure([
+			'error',
+			'data' => [
+				$this->structure
+			]
+		])->seeJson([
+			'error' => null,
+			'id' => $sale->id
+		]);
 
-        /**
-         * Test non existing sale
-         */
-        $this->get('/api/sales/NaN' )->seeJsonStructure([
-            'error' => [
-                'code',
-                'message',
-                'trace'
-            ],
-            'data' => []
-        ])->seeJson([
-            'code' => 404
-        ]);
-    }
+		/**
+		 * Test non existing sale
+		 */
+		$this->get('/api/sales/NaN')->seeJsonStructure([
+			'error' => [
+				'code',
+				'message',
+				'trace'
+			],
+			'data' => []
+		])->seeJson([
+			'code' => 404
+		]);
+	}
+
+	/**
+	 * Test create/delete a sale
+	 *
+	 * @return void
+	 */
+	public function testCreateSale()
+	{
+		$sale = factory(\App\Models\Sales::class, 1)->make();
+
+		$result = $this->post('/api/sales/', $sale->getAttributes())->seeJsonStructure([
+			'error',
+			'data' => [
+				$this->structure
+			]
+		])->seeJson([
+			'error' => null,
+		])->decodeResponseJson();
+		$resultData = $result['data'][0];
+
+		/**
+		 * Remove the newly created sale just to keep it clean
+		 */
+		$this->delete('/api/sales/' . $resultData['id'])->seeJsonStructure([
+			'error',
+			'data' => [
+				$this->structure
+			]
+		])->seeJson([
+			'error' => null,
+			'id' => $resultData['id']
+		]);
+
+
+		/**
+		 * Test general validations error
+		 */
+		$sale = new \App\Models\Sales();
+		$this->post('/api/sales/', $sale->getAttributes())->seeJsonStructure([
+			'error' => [
+				'code',
+				'message',
+				'trace'
+			],
+			'data' => []
+		])->seeJson([
+			'message' => "Validation Error.",
+			'code' => 422
+		]);
+	}
+
 }
