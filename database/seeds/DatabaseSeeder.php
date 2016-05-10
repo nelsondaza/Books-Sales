f0f217e5b99015cca328fd8255f28931ef0221fa
Nelson Daza <nelson.daza@gmail.com>
2016-05-09 21:55:58 -0500

    Added Seeders artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// $this->call(UsersTableSeeder::class);
+
+		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+		\App\Models\Sales::truncate();
+		\App\Models\Books::truncate();
+
+		factory(\App\Models\Books::class, 50)->create();
+		factory(\App\Models\Sales::class, 200)->create();
+
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+	}
 }
